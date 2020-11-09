13e651c216d93e929bd8613e1c10b0fdea4ecd6e
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-08 20:59:17 -0500

    User and Comment entity created

---
+++ src/entities/Comment.ts
@@ -0,0 +1,7 @@
+interface Comment {
+  id: string;
+  content: string;
+  createdAt: Date;
+}
+
+export default Comment;
---
+++ src/entities/User.ts
@@ -0,0 +1,8 @@
+interface User {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+}
+
+export default User;
