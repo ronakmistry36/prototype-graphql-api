8dc672760baec2daa9a4168c8049d93f22e95799
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-08 20:44:36 -0500

    Mysql connected

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 
-const { PORT } = process.env;
+// Configuration
+dotenv.config();
 const app = express();
 
-dotenv.config();
+export const {
+  PORT,
+  DATABASE_NAME, 
+  DATABASE_USER_NAME, 
+  DATABASE_PASSWORD, 
+  DATABASE_HOTS, 
+  DATABASE_PORT, 
+  DATABASE_DIALECT 
+} = process.env;
+
+const sequelize = new Sequelize(
+  DATABASE_NAME as string, 
+  DATABASE_USER_NAME as string, 
+  DATABASE_PASSWORD as string, 
+  {
+    host: 'localhost',
+    port: 8006,
+    dialect: 'mysql'
+  }
+);
+
 
 app.listen(PORT, () => {});
 
