f5843270f0068de1e86c86afe6a40d531de62a55
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-08 21:42:33 -0500

    TypeORM configured

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
-export const {
-  PORT,
-  DATABASE_NAME, 
-  DATABASE_USER_NAME, 
-  DATABASE_PASSWORD, 
-  DATABASE_HOTS, 
-  DATABASE_PORT, 
-  DATABASE_DIALECT 
-} = process.env;
+const { PORT } = process.env;
 
 app.listen(PORT, () => {});
 
