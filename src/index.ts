82498df645e998dfe5437999ed713c133e148ea3
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-17 21:40:35 -0500

    Work in progress

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { ConnectionOptions, getConnectionManager } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-import TypeORMUserRepository from './typeORM/TypeORMUserRepository';
+import TypeORMUserEntitySchema from './typeORM/TypeORMUserRepository';
 import UserEntity from './entity/UserEntity';
 
 // Configuration
 dotenv.config();
 const app = express();
 
-const {
-  PORT,
-  DATABASE_NAME,
-  DATABASE_USER_NAME,
-  DATABASE_PASSWORD,
-  DATABASE_HOST,
-  DATABASE_PORT,
-  DATABASE_DIALECT,
-} = process.env;
-
-const connectionOptions: ConnectionOptions = {
-  type: DATABASE_DIALECT as any,
-  host: DATABASE_HOST as string,
-  port: Number.parseFloat(DATABASE_PORT as string),
-  username: DATABASE_USER_NAME as string,
-  password: DATABASE_PASSWORD as string,
-  database: DATABASE_NAME as string,
-  synchronize: true,
-};
+const { PORT } = process.env;
 
 app.listen(PORT, async () => {
-  const connectionManager = getConnectionManager();
-  const connection = connectionManager.create(connectionOptions);
-  await connection.connect();
+  try {
+    const connection = await createConnection();
+    const userRepository = connection.getRepository(TypeORMUserEntitySchema);
+    userRepository.save({
+    });
+  } catch (error) {
+    // console.log(error);
+  }
 
   // const userRepository = new TypeORMUserRepository();
-
-  // const user:UserEntity = {
-  //   id: 'first-sample-id',
-  //   email: 'me@facebook.com',
-  //   firstName: 'React',
-  //   lastName: 'Facebook',
-  // };
-
   // userRepository.save(user);
 });
 
