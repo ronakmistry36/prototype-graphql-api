3e8ca0abc0ededf2c22e6b53d086983345f67b54
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-23 21:34:34 -0500

    GraphQL configured

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { graphqlHTTP } from 'express-graphql';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 const { PORT } = process.env;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: {},
+    graphiql: true
+  })
+)
 
 app.listen(PORT, async() => {
   console.log("App is running on port::", PORT)
