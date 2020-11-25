1ea17fbdb8d41581ea044964c292cd3292aea02a
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-24 20:05:09 -0500

    Basic express server configured

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,13 @@
-import express from 'express';
-// @ts-ignore
-import dotenv from 'dotenv';
-import "reflect-metadata";
-import {ApolloServer} from 'apollo-server';
-import {createConnection} from "typeorm";
-import {buildSchema} from "type-graphql";
-import {UserResolver} from "./graphQl/UserResolver";
+import * as express from 'express';
 
-
-dotenv.config();
 const app = express();
-app.use(express.json());
 
-const { PORT } = process.env;
+const PORT = 8001;
 
-const initializeApp = async () => {
-  const connection = await createConnection();
-  const schema = await buildSchema({ resolvers: [ UserResolver ]});
-  const apolloServer = new ApolloServer({ schema });
-  await apolloServer.listen(PORT);
+app.listen(PORT, async () => {
   console.log("App is running on port::", PORT);
-};
+});
 
-initializeApp();
+app.get('/', (_request, response) => {
+  response.send('Welcome');
+});
