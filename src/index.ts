f0848ae19bc7270cc1230711af6268fe4892e2cd
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-24 20:06:11 -0500

    Env configured

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as express from 'express';
+import 'dotenv/config';
 
 const app = express();
 
-const PORT = 8001;
+const {PORT} = process.env;
 
 app.listen(PORT, async () => {
   console.log("App is running on port::", PORT);
