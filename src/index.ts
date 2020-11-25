0128a212c564513f310a338f3a72c80bda94d8cb
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-24 20:08:51 -0500

    Db connection created

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as express from 'express';
 import 'dotenv/config';
+import {createConnection} from "typeorm";
 
 const app = express();
 
 const {PORT} = process.env;
 
 app.listen(PORT, async () => {
+  await createConnection();
   console.log("App is running on port::", PORT);
 });
 
